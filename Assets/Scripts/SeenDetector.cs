13e1fa002acf0fa8ef9a828efd7b369e157d2d6b
nliebers <nliebers24@gmail.com>
2022-02-08 19:26:53 -0600

    Adjusted dog AI.

--- Assets/Scripts/SeenDetector.cs
+++ Assets/Scripts/SeenDetector.cs
@@ -5,10 +5,12 @@ using UnityEngine;
 public class SeenDetector : MonoBehaviour
 {
 	public bool seen = false;
+	public GameObject detector;
 	
     void OnTriggerEnter(Collider other) {
         if (other.gameObject.tag == "Player") {
             seen = true;
+			detector.GetComponent<BoxCollider>().enabled = false;
         }
     }
 }
